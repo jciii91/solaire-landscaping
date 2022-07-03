ffeab56c3e2b9ac930c29e9045e056e600534579
John Costello <jcostello711@gmail.com>
2022-07-02 20:34:22 -0400

    gallery conditionally renders photos based on category selection

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,14 @@
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Gallery(props) {
+  const { currentCategory } = props;
+  return (
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
+}
+export default Gallery;
