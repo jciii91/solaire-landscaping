3ab8b3e9976bd3b5fb747fb90afdf5c6edc0dc1f
John Costello <jcostello711@gmail.com>
2022-07-03 18:48:09 -0400

    contact form added. input validation added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
@@ -24,6 +25,7 @@ function App() {
       ></Nav>
       <main>
         <div>
+          <ContactForm></ContactForm>
           <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
