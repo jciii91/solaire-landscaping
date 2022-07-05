6e546d5df9ed22f6d28580658cc0fa5490d4669a
John Costello <jcostello711@gmail.com>
2022-07-04 22:09:37 -0400

    Gallery condensed

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,35 +18,28 @@ function Nav(props) {
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera"> ☀️</span> Solaire Landscaping
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
             <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-              About me
+              Services Offered
             </a>
+          </li>
+          <li className="mx-2">
+            <span onClick={() => {
+                setCurrentCategory(categories[0]);
+                setContactSelected(false);
+              }}
+            >
+              Gallery
+            </span>
           </li>
           <li className={`mx-2 ${contactSelected && 'navActive'}`}>
             <span onClick={() => setContactSelected(true)} >Contact</span>
           </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && `navActive`
-                }`}
-              key={category.name}
-            >
-              <span onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
         </ul>
       </nav>
     </header>
